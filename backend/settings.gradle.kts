b70971c80cddf0c0be92e950a432f11c13dc3bee
MK00098 <pilot98g@gmail.com>
2025-10-01 17:45:13 +0900

    Add files via upload

---
+++ backend/settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+val dmAuthToken: String by settings
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven {
+            url = uri("https://jitpack.io")
+            credentials {
+                username = dmAuthToken
+            }
+        }
+    }
+}
+
+rootProject.name = "biosensor"
+include(":app")
+ 
